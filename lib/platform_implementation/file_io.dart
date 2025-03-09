01e9803e5789d187e07672b729149253f297182c
Engr., Isaiah Pius <isaiahtech1@gmail.com>
2025-03-09 14:52:07 +0100

    fixed documentation needs

--- lib/platform_implementation/file_io.dart
+++ lib/platform_implementation/file_io.dart
@@ -3,12 +3,17 @@ import 'dart:io';
 
 /// Standard Native Platform File helper
 class FileHelper {
+
+  /// wrapper for native file.exists
   static Future<bool> fileExists(String path) async {
     return File(path).exists();
   }
 }
 
+/// File manager for native File API
 class FileManager {
+
+  /// returns File of the platform
   static File readFile(String path) {
     return File(path);
   }
--- lib/platform_implementation/platform_web.dart
+++ lib/platform_implementation/platform_web.dart
@@ -1,5 +1,7 @@
 /// Platform wrapper for web platforms
 class PlatformHelper {
+
+  /// Always false for web
   static bool get isIOS {
     return false; // Web is not iOS
   }
